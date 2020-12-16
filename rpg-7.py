3c3dce57da798adba8d351008f5b5f89956f17cc
crohn44 <notmeae@gmail.com>
2020-12-16 11:25:18 -0500

    Added medic class and created a character selection

--- rpg-7.py
+++ rpg-7.py
@@ -7,6 +7,8 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 
 """
 
+import random
+
 class Character:
     def __init__(self, health, power):
         self.health = health
@@ -27,16 +29,57 @@ class Character:
     def print_status(self):
         print("The %s has %d health and %d power." % (self, self.health, self.power))
 
-class Hero(Character):
+class Warrior(Character):
     def __str__(self):
-        return "Hero"
+        return "Warrior"
+
+    def attack(self, opponent):
+        modifier = random.randint(1, 5)
+        if modifier == 5:
+            bonus_damage = 2
+        else:
+            bonus_damage = 1
+        opponent.health -= self.power*bonus_damage
+        print("The %s does %d damage to the %s." % (self, self.power*bonus_damage, opponent))
+        opponent.alive()
+        if opponent.isalive == False:
+            print("The %s is dead." % opponent)
+
+class Medic(Character):
+    def __str__(self):
+        return "Medic"
+    
+    def alive(self):
+        modifier = random.randint(1, 5)
+        if modifier == 5:
+            print('The Medic recovered 2 hp!')
+            self.health += 2
+        if self.health < 1:
+            self.isalive = False
+        
+            
 
 class Goblin(Character):
     def __str__(self):
         return "Goblin"
 
-hero = Hero(10, 5)
 goblin = Goblin(6, 2)
+hero = Character(0, 0)
+
+def character_select():
+    while True:
+        print('Choose your character:')
+        print('1. Warrior')
+        print('2. Medic')
+        selection = input()
+        if selection == '1':
+            print('You chose the Warrior class!')
+            return Warrior(10, 5)
+        elif selection == '2':
+            print('You chose the Medic class!')
+            return Medic(8, 4)
+        else:
+            print("Invalid input %r" % selection)
 
 def main():
 
@@ -66,4 +109,5 @@ def main():
         
         print()
 
+hero = character_select() 
 main()
